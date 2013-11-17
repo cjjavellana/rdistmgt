596fdd5b77edc06e4b27bcfd8ce02e7340a7c3b6
Christian Javellana <cjjavellana@gmail.com>
2013-11-17 12:14:38 +0800

    Initial Commit

---
+++ client/client_main.js
@@ -0,0 +1,8 @@
+Template.siteHeader.redLogo = function() {
+  return "Relief Distribution Management";
+}
+
+Template.footer.footerMsg = function() {
+  return "(c) 2013 Philippine National Red Cross";
+}
+
